8f1b9c202b17c2261ee5bbabe61e6e4900f172d1
AnaKarolyneLima <lemos.karolayne@gmail.com>
2025-03-18 23:15:38 -0300

    enviar projeto

---
+++ SistemaBancario/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+
+namespace SistemaBancario
+{
+     class ContaBancaria
+    {
+       private int _numeroConta;
+       private string _titular { get; set; }
+       private double _saldo { get; set; }
+
+        public ContaBancaria (int numeroConta, string titular, double saldoInicial)
+        {
+            this._numeroConta = numeroConta;
+            this._titular = titular;
+            this._saldo = saldoInicial;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Seu saldo é: {_saldo}");
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor <= _saldo && valor > 0)
+            {
+                _saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo Insuficiente ou valor inválido! ");
+
+            }
+        }
+    }
+
+    
+}
+
---
+++ SistemaBancario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaBancario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
